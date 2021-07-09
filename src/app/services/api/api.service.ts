67950c02dc3d348d00a1b173455a37a910f68296
dev-ptera <dev.ptera@gmail.com>
2021-07-09 14:52:21 -0400

    Add some timeouts for overloaded servers

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -13,6 +13,11 @@ import {
     RepresentativesResponseDto,
 } from '@app/types/dto';
 import { InsightsDto } from '@app/types/dto/InsightsDto';
+import { timeout } from 'rxjs/operators';
+
+const SLOW_MS = 20000;
+const MED_MS = 15000;
+const FAST_MS = 10000;
 
 @Injectable({
     providedIn: 'root',
@@ -24,7 +29,10 @@ export class ApiService {
     constructor(private readonly _http: HttpClient) {}
 
     accountOverview(address: string): Promise<AccountOverviewDto> {
-        return this._http.get<AccountOverviewDto>(`${this.url}/account-overview/${address}`).toPromise();
+        return this._http
+            .get<AccountOverviewDto>(`${this.url}/account-overview/${address}`)
+            .pipe(timeout(SLOW_MS))
+            .toPromise();
     }
 
     confirmedTransactions(address: string, offset: number, pageSize: number): Promise<ConfirmedTransactionDto[]> {
@@ -32,52 +40,61 @@ export class ApiService {
             .get<ConfirmedTransactionDto[]>(
                 `${this.url}/confirmed-transactions?address=${address}&offset=${offset}&size=${pageSize}`
             )
+            .pipe(timeout(MED_MS))
             .toPromise();
     }
 
     block(hash: string): Promise<BlockDto> {
-        return this._http.get<BlockDto>(`${this.url}/block/${hash}`).toPromise();
+        return this._http.get<BlockDto>(`${this.url}/block/${hash}`).pipe(timeout(FAST_MS)).toPromise();
     }
 
     node(): Promise<MonitoredRepDto> {
-        return this._http.get<MonitoredRepDto>(`${this.url}/node`).toPromise();
+        return this._http.get<MonitoredRepDto>(`${this.url}/node`).pipe(timeout(FAST_MS)).toPromise();
     }
 
     monkey(address: string): Promise<string> {
         const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
         return this._http
             .get(`https://monkey.banano.cc/api/v1/monkey/${address}`, { headers, responseType: 'text' })
+            .pipe(timeout(FAST_MS))
             .toPromise<string>();
     }
 
     representatives(): Promise<RepresentativesResponseDto> {
-        return this._http.get<RepresentativesResponseDto>(`${this.url}/representatives`).toPromise();
+        return this._http
+            .get<RepresentativesResponseDto>(`${this.url}/representatives`)
+            .pipe(timeout(MED_MS))
+            .toPromise();
     }
 
     /* Rich List is too expensive operation to run non-locally; default to production. */
     bananoDistribution(): Promise<AccountDistributionStatsDto> {
-        return this._http.get<AccountDistributionStatsDto>(`${this.richListUrl}/accounts-distribution`).toPromise();
+        return this._http
+            .get<AccountDistributionStatsDto>(`${this.richListUrl}/accounts-distribution`)
+            .pipe(timeout(MED_MS))
+            .toPromise();
     }
 
     getAccountBalances(offset: number, pageSize: number): Promise<AccountBalanceDto[]> {
         return this._http
             .get<AccountBalanceDto[]>(`${this.richListUrl}/accounts-balance?offset=${offset}&size=${pageSize}`)
+            .pipe(timeout(3000))
             .toPromise();
     }
 
     getPriceInfo(): Promise<PriceDataDto> {
-        return this._http.get<PriceDataDto>(`${this.url}/price`).toPromise();
+        return this._http.get<PriceDataDto>(`${this.url}/price`).pipe(timeout(FAST_MS)).toPromise();
     }
 
     getInsights(address: string): Promise<InsightsDto> {
-        return this._http.get<InsightsDto>(`${this.url}/insights/${address}`).toPromise();
+        return this._http.get<InsightsDto>(`${this.url}/insights/${address}`).pipe(timeout(SLOW_MS)).toPromise();
     }
 
     getOnlineReps(): Promise<string[]> {
-        return this._http.get<string[]>(`${this.url}/online-reps`).toPromise();
+        return this._http.get<string[]>(`${this.url}/online-reps`).pipe(timeout(FAST_MS)).toPromise();
     }
 
     getKnownAccounts(): Promise<KnownAccountDto[]> {
-        return this._http.get<KnownAccountDto[]>(`${this.url}/known-accounts`).toPromise();
+        return this._http.get<KnownAccountDto[]>(`${this.url}/known-accounts`).pipe(timeout(FAST_MS)).toPromise();
     }
 }
