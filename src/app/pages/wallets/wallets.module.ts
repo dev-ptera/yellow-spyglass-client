ac3321c8680474a254215bc0ee9fa279e7016b57
dev-ptera <dev.ptera@gmail.com>
2021-07-28 14:30:41 -0400

    Run prettier

--- src/app/pages/wallets/wallets.module.ts
+++ src/app/pages/wallets/wallets.module.ts
@@ -6,11 +6,19 @@ import { MatTableModule } from '@angular/material/table';
 import { MatBadgeModule } from '@angular/material/badge';
 import { AppCommonModule } from '@app/common/app-common.module';
 import { MatButtonModule } from '@angular/material/button';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [WalletsComponent],
-    imports: [CommonModule, AppCommonModule, HighchartsChartModule, MatBadgeModule, MatTableModule, MatButtonModule, RouterModule],
+    imports: [
+        CommonModule,
+        AppCommonModule,
+        HighchartsChartModule,
+        MatBadgeModule,
+        MatTableModule,
+        MatButtonModule,
+        RouterModule,
+    ],
     exports: [WalletsComponent],
 })
 export class WalletsModule {}
