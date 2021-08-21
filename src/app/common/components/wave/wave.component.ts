04fdb0aa1bea6e4e4e2d58f71e7b85908f0c65ea
dev-ptera <dev.ptera@gmail.com>
2021-08-20 23:31:37 -0400

    Continue style changes for home screen

--- src/app/common/components/wave/wave.component.ts
+++ src/app/common/components/wave/wave.component.ts
@@ -4,13 +4,19 @@ import { ViewportService } from '@app/services/viewport/viewport.service';
 @Component({
     selector: 'app-wave',
     template: `
-        <div *ngIf="!vp.sm" style="position: fixed; bottom: 0px; width: 100%; z-index: -1; min-width: 1200px">
+        <div *ngIf="!vp.sm" style="position: fixed; width: 100%; z-index: -1; min-width: 1200px"
+            [style.bottom.px]="bottom"
+             [style.maxHeight.px]="maxHeight"
+
+        >
             <img [src]="'assets/waves/wave' + waveNumber + '.svg'" />
         </div>
     `,
 })
 export class WaveComponent {
     @Input() waveNumber: number = 5;
+    @Input() maxHeight: number;
+    @Input() bottom: number = 0;
 
     constructor(public vp: ViewportService) {}
 }
