dd06deace0122bc63912d19d0866f540d7f0cfb2
dev-ptera <dev.ptera@gmail.com>
2022-05-15 11:42:54 -0400

    Use Spyglass API price endpoint

--- src/app/services/price/price.service.ts
+++ src/app/services/price/price.service.ts
@@ -5,17 +5,18 @@ import { PriceDataDto } from '@app/types/dto';
 @Injectable({
     providedIn: 'root',
 })
+/** Fetches price data & refreshes every 5 minutes. */
 export class PriceService {
     priceData: PriceDataDto;
 
     constructor(private readonly _api: ApiService) {
-        this.refreshPriceData();
+        this._refreshPriceData();
         setInterval(() => {
-            this.refreshPriceData();
+            this._refreshPriceData();
         }, 60000 * 5);
     }
 
-    refreshPriceData(): void {
+    private _refreshPriceData(): void {
         this._api
             .fetchPriceInfo()
             .then((data: PriceDataDto) => {
