bfbf574fc45ef896c13fb4ec15112959aa2a5887
dev-ptera <dev.ptera@gmail.com>
2022-05-23 14:32:03 -0400

    Re-add file I need

--- src/app/common/app-common.module.ts
+++ src/app/common/app-common.module.ts
@@ -5,6 +5,7 @@ import { EmptyStateModule, SpacerModule } from '@brightlayer-ui/angular-componen
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ResponsiveDirective } from '@app/common/directives/responsive.directive';
+import { SafeHtmlPipe } from '@app/common/pipes/safe.pipe';
 import { BookmarkButtonComponent } from '@app/common/components/bookmark-button/bookmark-button.component';
 import { CopyButtonComponent } from '@app/common/components/copy-button/copy-button.component';
 import { QrButtonComponent } from '@app/common/components/qr-button/qr-button.component';
@@ -26,6 +27,7 @@ import { PercentagePipe } from '@app/common/pipes/percentage.pipe';
         PercentagePipe,
         ErrorComponent,
         ResponsiveDirective,
+        SafeHtmlPipe,
     ],
     imports: [CommonModule, EmptyStateModule, MatButtonModule, MatIconModule, MatSnackBarModule, SpacerModule],
     entryComponents: [QrDialogComponent],
@@ -39,6 +41,7 @@ import { PercentagePipe } from '@app/common/pipes/percentage.pipe';
         PercentagePipe,
         ErrorComponent,
         ResponsiveDirective,
+        SafeHtmlPipe,
     ],
 })
 export class AppCommonModule {}
